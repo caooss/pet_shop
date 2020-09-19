13a2aeec8b6b31e8817de4a4d93a746b415f04d7
caooss <rafael.r.l.pavan@gmail.com>
2020-09-18 23:14:07 -0300

    Criado o formulario de cadastro e o envio para o banco de dados dos animais

---
+++ cad_animal.php
@@ -0,0 +1,15 @@
+<?php
+    include("./inc/cabecalho_conexao.php");
+
+    $tipo=$_POST["tipo"];
+    $raca=$_POST["raca"];
+    $nome=$_POST["nome"];
+    $dono_rg=$_POST["dono_rg"];
+
+    $sql="INSERT INTO animal (raca, nome, tipo, dono_rg)
+          VALUE('$raca', '$nome', '$tipo', $dono_rg)";
+
+    $txt="Animal cadastrado com sucesso!";
+
+    include("./inc/rodape_conexao.php");
+?>
